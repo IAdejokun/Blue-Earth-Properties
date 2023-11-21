fa5f6db40474b78c82a19d4933cf67de4d803b85
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-11-21 12:09:55 +0000

    modules added and configurations made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SellComponent } from './pages/sell/sell.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CardComponent } from './components/card/card.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TeamComponent } from './pages/about/team/team.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule} from '@angular/material/menu';
+import {register} from 'swiper/element/bundle';
+import { WhoWeAreComponent } from './pages/about/who-we-are/who-we-are.component';
+import { VisionMissionComponent } from './pages/about/vision-mission/vision-mission.component';
+import { MeetBrokerComponent } from './pages/about/meet-broker/meet-broker.component';
 
+register()
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    SellComponent,
+    ContactComponent,
+    CardComponent,
+    AboutComponent,
+    TeamComponent,
+    PageNotFoundComponent,
+    WhoWeAreComponent,
+    VisionMissionComponent,
+    MeetBrokerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
